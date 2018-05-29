1d10c98f9ae44b869c9298fd17824f95fc3a0065
Wolfmercury <kranz.kilian@outlook.de>
2018-05-29 20:34:31 +0200

    Alles umgeworfen

--- Abgabe 3/Arbeitsumgebung/numerik3.py
+++ Abgabe 3/Arbeitsumgebung/numerik3.py
@@ -20,52 +20,27 @@ N_0 = 8.1e25                #Anzahl an momentanen CO2 molekuelen in luftsaule mi
 
 
 class treibhaus:
-    #input: Zahlenwert x, Zahlenwert y
-    #output: Multiplikation davon
-    #rechnet triviale rechnung
-    def x_i(x,y):
-        return x*y
-    
     #input: Temperatur T, Wellenlaenge nu
     #output: rho
     #berechnung der spektralen Energiedichte fuer unpolarisierte Strahlung
     def rho(nu,T):
-        Rechnung = ((8*np.pi*planck*nu**3)/c**3) * 1/(np.exp((planck*nu)/(k*T))-1)
+        Rechnung = ((8*np.pi*planck*nu**3)/(c**3)) * 1/(np.exp((planck*nu)/(k*T))-1)
         return Rechnung   
-    
-    #input: Temperatur T, Wellenlaenge nu
-    #output: rho
-    #berechnung der Ableitung der spektralen Energiedichte fuer unpolarisierte Strahlung
-    def rho_diff(nu,T):
-        Rechnung = (8*np.pi*planck*nu**3)/c**3 * planck*nu*np.exp(planck*nu/(k*T))/(k*T**2*(np.exp(planck*nu/(k*T))-1)**2)
-        return Rechnung
     
     #input: Frequent nu
     #output: Wirkungsquerschnitt sigma
     #Formel für Wirkungsquerschnitt
     def sigma(nu):
-        Wert = (S_2/np.pi*(Gamma/(((nu-nu_2)**2)+Gamma**2))) + (S_3/np.pi*(Gamma/(((nu-nu_3)**2)+Gamma**2)))
+        Wert = (S_2/np.pi)*Gamma/((nu-nu_2)**2+Gamma**2) + (S_3/np.pi)*Gamma/((nu-nu_3)**2+Gamma**2)
         return Wert
     
     #input: n_schlange (hier eta) und nu
     #output: Wahscheinlichkeit fuer das Entweichen eines Photons der Frequenz nu (f)
     #funktion im Integral    
     def f(eta,nu):
-        return np.exp(-(eta*N_0*treibhaus.sigma(nu)))
+        Wert = np.exp(-(eta*N_0*treibhaus.sigma(nu)))
+        return Wert
         
-    #input:Genauigkeit n, Temperaut T, länge l des Intervalls
-    #output: Wert für bestimmte Genauigkeit
-    #analyitisches Integral (epsilon(T))
-    def Integral(l,n,T,eta):
-        h = l/n     #stützstellen breite
-        i = 1       #laufindex
-        summe = 0   #gewünschte numerische Summe des Integrals
-        while treibhaus.x_i(h,i) < n:
-            summe += h*treibhaus.rho(treibhaus.x_i(h,i),T)*(1-treibhaus.f(eta,treibhaus.x_i(h,i)))
-            i += 1
-            #print(summe)
-        return summe
-
     #Für das Integral nach Simpsons benötigte hilfsfunktion
     def Simpson_Fkt(nu,eta,T):
         Wert = treibhaus.rho(nu,T)*(1-treibhaus.f(eta,nu))
@@ -75,33 +50,18 @@ class treibhaus:
     #output: Integral
     #numerische Berechnung des Integrals nach Simpson
     def Integral_S(l,n,T,eta):
-        h = l / n       #Schrittweite
-        #Summe = treibhaus.Simpson_Fkt(0,eta,T)
-        Summe = 0
-        i = h       #laufindex
-        while i < n:
+        h = l / n           #Schrittweite
+        Summe = 0   
+        i = h               #laufindex
+        while i < n:    #bis zur vorletzten Schrittweite
             if i % 2 == 0:  #Gerader Koeffizient    (für Gewichtung)
                 Summe += 4*treibhaus.Simpson_Fkt(i,eta,T)
             else:           #ungerade Koeffizient
                 Summe += 2*treibhaus.Simpson_Fkt(i,eta,T)
-            i += h  #um Schrittweise höhere Index
-        Summe += treibhaus.Simpson_Fkt(n,eta,T)
-        return Summe
-        
-
-
-    #input:Genauigkeit n, Temperaut T, länge l des Intervalls
-    #output: Wert für bestimmte Genauigkeit
-    #Ableitung des analyitisches Integral, für das Newton Kriterium (epsilon(T)')
-    def Integral_diff(l,n,T,eta):
-        h = l/n     #stützstellen breite
-        i = 1       #laufindex
-        summe = 0   #gewünschte numerische Summe des Integrals
-        while treibhaus.x_i(h,i) < n:
-            summe += h*treibhaus.rho_diff(treibhaus.x_i(h,i),T)*(1-treibhaus.f(eta,treibhaus.x_i(h,i)))
-            i += 1
-        return summe
-        
+            i += h          #um Schrittweise höhere Index
+        Summe += treibhaus.Simpson_Fkt(l,eta,T) #aufsummieren der Stützwerte am Beginn und am Ende der Fkt. (Beginn hier 0)
+        return (h/3)*Summe
+         
     #input: Temperatur T und gewünschte Genauigkeit
     #Output: 1/analytische Lösung des Integrals über die spektrale Energiedichte rho
     #analytisch geslöstes Integral wird bestimmt mit einer Genauigkeit, die gegeben wird(aber 1/Z)
@@ -110,72 +70,36 @@ class treibhaus:
         Wert = 0    #wird aktuallisiert für die Durchläufe
         m = 1       #Startwert der Näherung
         while (Z-Wert)/Z > Genauigkeit:
-            if m == 1:
+            if m == 1:   #devision by zero umgehung
                 Z = 0
             Wert = Z
-            Z += (48*np.pi*k**4*T**4)/(c**3*planck**3*m**4)
+            Z += (48*np.pi*k**4*T**4)/(c**3*planck**3*m**4) #Summand der analytischen Lösung
             m += 1
         return 1/Z
-        
-    #Analztisches Integral von Z ableiten
-    def Z_Integral_diff(Genauigkeit,T):
-        Z = 1       #division by zero umgehen in while Schleife Kriterium  
-        Wert = 0    #wird aktuallisiert für die Durchläufe
-        m = 1       #Startwert der Näherung
-        while (Z-Wert)/Z > Genauigkeit:
-            if m == 1:
-                Z = 0
-            Wert = Z
-            Z += (192*np.pi*k**4*T**3)/(c**3*planck**3*m**4)
-            m += 1
-        return 1/Z
-        
-    #input: Startwerte: Genauigkeit i.A., Startschrittweite l, n ka, eta
-    #output:
-    #Newton Kriterium zum lösen der Gleichung mit einer Unbekannten
-    def Newton(l,n,Tstart,Genauigkeit,eta):
-        dT = 1      #Newtonkriterium
-        T = Tstart
-        while dT > Genauigkeit:
-            print(treibhaus.Integral_S(l,n,T,eta)*treibhaus.Z_Integral(Genauigkeit,T))
-            dT = treibhaus.Integral_S(l,n,T,eta)*treibhaus.Z_Integral(Genauigkeit,T) / (treibhaus.Integral_diff(l,n,T,eta)*treibhaus.Z_Integral(Genauigkeit,T)+treibhaus.Z_Integral_diff(Genauigkeit,T)*treibhaus.Integral_S(l,n,T,eta))
-            T -= dT
-            #print(T)
-        return T
-
     
+    #input: l: Breite des Intervalls, n: Anzahl Stützstellen (Gerade), T: Temperatur,Genauigkeit, eta: n_schlange 
+    #output: epsilon von gewünschter Temperatur und n_schlange 
+    #Funktion Epsilon, die das produktder analytischen Lösung von 1/Z und die numerischen nach Simpson berechnet    
     def epsilon(l,n,T,eta,Genauigkeit):
         Wert = treibhaus.Z_Integral(Genauigkeit,T)*treibhaus.Integral_S(l,n,T,eta)
         return Wert
     
-    
-    
-    def Fixpunktformel(l,n,Tstart,Genauigkeit,eta):
-        TE = 1      #=1 damit while schleife kein division by zero error
+    #input: l: Breite des Intervalls, n: Anzahl Stützstellen (Gerade), Tstart: Starttemperatur,Genauigkeit, eta: n_schlange 
+    #output: mittlere Erdtemperatur als Funktion von n schlange (tilde) in [K]
+    #Fixpunktgleichung zum Lösen der Gleichung mit einen Unbekannten    
+    def Fixpunkt(l,n,Tstart,Genauigkeit,eta):
+        TE = Tstart #=1 damit while schleife kein division by zero error
         Wert = 0
         T = Tstart
-        
         while (TE-Wert)/TE > Genauigkeit:
-            if T == Tstart: #devision by zero umgehen
-                TE = 0
             Wert = TE
             TE = T_S*((epsilon_S*(2-treibhaus.epsilon(l,n,T_S,eta,Genauigkeit)))/(2-treibhaus.epsilon(l,n,T,eta,Genauigkeit)))**(1/4)
-            print(TE-Wert)
             T = TE
-            print(T)
         return TE
         
 
 
-#print(treibhaus.Integral_S(20,100,1,1))
-#print(treibhaus.Z_Integral(1e-8,1))
-#print(treibhaus.Integral_S(20,100,1,1)*treibhaus.Z_Integral(1e-8,1))
-
-print(treibhaus.Fixpunktformel(1,10,1,1e-6,1))
-
-#print(treibhaus.Newton(1,10,1,1e-6,1))
-
-#BEI SUMME von INTEGRAL NUMERISCH DIE WHILE SCHLEIFE SO äNDERN WIE SONST AUCH
+print(treibhaus.Fixpunkt(10,50,100,1e-8,1))
 #eta:1: Zimmertemperatur
 #eta:2: groeßenordnung e8 ca
 
